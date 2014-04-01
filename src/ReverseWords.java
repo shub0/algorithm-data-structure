f17868cc210876a4bb59ad0a2b9ee1f98e0114da
Shubo <shubo@adap.tv>
2014-03-31 21:39:37 -0700

    [ReverseWords]
    Given an input string, reverse the string word by word.
    
    For example,
    Given s = "the sky is blue",
    return "blue is sky the".
    
    Clarification:
    What constitutes a word?
    A sequence of non-space characters constitutes a word.
    Could the input string contain leading or trailing spaces?
    Yes. However, your reversed string should not contain leading or trailing spaces.
    How about multiple spaces between two words?
    Reduce them to a single space in the reversed string.

---
+++ src/ReverseWords.java
@@ -0,0 +1,23 @@
+
+public class ReverseWords {
+	public static String reverseWords(String input) {
+        if (input == null || input.length() == 0) {
+			return "";
+		}
+		String[] words = input.split(" ");
+		StringBuilder output = new StringBuilder();
+		for (int index = words.length - 1; index > 0; index --) {
+			if (words[index].length() == 0)
+				continue;
+			output.append(words[index]);
+			output.append(" ");
+		}
+		if (words.length > 0)
+		    output.append(words[0]);
+		return output.toString().trim();
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(reverseWords(" 1"));
+	}
+}
