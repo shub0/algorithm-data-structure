a5f90f605db2452f0020e7fe8295f8dda0ed5b09
Shubo <shubo@adap.tv>
2014-04-02 21:49:43 -0700

    [atoi]
    This code has handle all corner case and pass leetcode test

--- src/Atoi.java
+++ src/Atoi.java
@@ -1,39 +1,41 @@
 
 public class Atoi {
 	public int atoi(String str) {
-		if (str == null || str.trim().length() == 0) {
+		if (str == null || str.length() < 1)
 			return 0;
-		} else if (str.trim().charAt(0) == '-') {
-			return atoi(str.trim().substring(1), -1);
-		} else if (str.trim().charAt(0) == '+') {
-			return atoi(str.trim().substring(1), 1);
-		} else {
-			return atoi(str.trim(), 1);
+	 
+		// trim white spaces
+		str = str.trim();
+	 
+		char flag = '+';
+	 
+		// check negative or positive
+		int i = 0;
+		if (str.charAt(0) == '-') {
+			flag = '-';
+			i++;
+		} else if (str.charAt(0) == '+') {
+			i++;
 		}
-	}
-
-	private int atoi(String str, int sign) {
-		int INT_MAX = 2147483647;
-		int INT_MIN = -2147483648;
-		int output = 0;
-		int len = str.length();
-		for (int index = 0; index < len; index ++) {
-			char digit = str.charAt(index);
-			if (digit > '9' || digit < '0')
-				return sign * output;
-			output *= 10;
-			output += (int)(digit - '0');
-			if (output < 0) {
-				if (sign < 0) {
-					return INT_MIN;
-				} else {
-					return INT_MAX;
-				}
-			}
+		// use double to store result
+		double result = 0;
+	 
+		// calculate value
+		while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+			result = result * 10 + (str.charAt(i) - '0');
+			i++;
 		}
-		return sign * output;
-	}
-	public static void main(String[] args) {
-		System.out.println(new Atoi().atoi("  - 321"));
+	 
+		if (flag == '-')
+			result = -result;
+	 
+		// handle max and min
+		if (result > Integer.MAX_VALUE)
+			return Integer.MAX_VALUE;
+	 
+		if (result < Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+	 
+		return (int) result;
 	}
 }
