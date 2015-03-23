2724e14787895b5da8883b803e51778907458e10
Shubo Liu <shubo.liu.mail@gmail.com>
2015-03-22 22:52:37 -0700

    more solution

--- python/combinations_sum.py
+++ python/combinations_sum.py
@@ -16,48 +16,73 @@ A solution set is:
 '''
 
 class Solution:
-    # @param candidates, a list of integers
-    # @param target, integer
-    # @return a list of lists of integers
-    def combinationSumShort(self, candidates, target):
-        output = []
-        candidates.sort()
-        for index in range(len(candidates)):
-            num = candidates[index]
-            if target > num:
-                sub_output = self.combinationSum(candidates[index:], target -  num)
-                if len(sub_output) > 0:
-                    output += [ [num] + a_list for a_list in sub_output ]
-            elif target == num:
-                output += [ [num] ]
-            else:
-                break
-        return output
+	# @param candidates, a list of integers
+	# @param target, integer
+	# @return a list of lists of integers
+	def combinationSumShort(self, candidates, target):
+		output = []
+		candidates.sort()
+		for index in range(len(candidates)):
+			num = candidates[index]
+			if target > num:
+				sub_output = self.combinationSum(candidates[index:], target -  num)
+				if len(sub_output) > 0:
+					output += [ [num] + a_list for a_list in sub_output ]
+				elif target == num:
+					output += [ [num] ]
+				else:
+					break
+		return output
 
-    # @param candidates, a list of integers
-    # @param target, integer
-    # @return a list of lists of integers
-    def combinationSum(self, candidates, target):
-        output = []
-        size = len(candidates)
-        if size == 0:
-            return output
-        candidates.sort()
-        self.make_combination(candidates, target, 0, list(), output)
-        return output
+	# @param candidates, a list of integers
+	# @param target, integer
+	# @return a list of lists of integers
+	def combinationSum(self, candidates, target):
+		output = []
+		size = len(candidates)
+		if size == 0:
+			return output
+		candidates.sort()
+		self.make_combination(candidates, target, 0, list(), output)
+		return output
 
-    def make_combination(self, candidates, target,start_index, a_list, output):
-        if target == 0:
-            output.append(a_list[:])
-        else:
-            size = len(candidates)
-            for index in range(start_index, size):
-                if target < candidates[index]:
-                    break
-                a_list.append(candidates[index])
-                self.make_combination(candidates, target - candidates[index], index, a_list, output)
-                a_list.pop()
+	def make_combination(self, candidates, target,start_index, a_list, output):
+		if target == 0:
+			output.append(a_list[:])
+		else:
+			size = len(candidates)
+			for index in range(start_index, size):
+				if target < candidates[index]:
+					break
+				a_list.append(candidates[index])
+				self.make_combination(candidates, target - candidates[index], index, a_list, output)
+				a_list.pop()
+
+	# @param candidates, a list of integers
+	# @param target, integer
+	# @return a list of lists of integers
+	def combinationSum2(self, candidates, target):
+		output = []
+		size = len(candidates)
+		if size == 0:
+			return output
+		candidates.sort()
+		self.make_combinations2(candidates, target, 0, list(), output)
+		list_set = { tuple(a_list) for a_list in output }
+		return [list(a_tuple) for a_tuple in list_set]
+
+	def make_combinations2(self, candidates, target, start_index, a_list, output):
+		if target == 0:
+			output.append(a_list[:])
+		else:
+			size = len(candidates)
+			for index in range(start_index, size):
+				if target < candidates[index]:
+					break
+				a_list.append(candidates[index])
+				self.make_combinations2(candidates, target-candidates[index], index + 1, a_list, output)
+				a_list.pop()
 
 if __name__ == '__main__':
     solution = Solution()
-    print solution.combinationSum([48,22,49,24,26,47,33,40,37,39,31,46,36,43,45,34,28,20,29,25,41,32,23], 69)
+    print solution.combinationSum2([1,1],1)
