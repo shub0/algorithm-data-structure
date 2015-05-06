dea22d294369fc8c0e6f3a6535be2940b2392827
shubo <shubo@adap.tv>
2015-05-06 00:09:22 -0700

    permutation

--- python/next_permutation.py
+++ python/next_permutation.py
@@ -11,6 +11,10 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 1,2,3 -> 1,3,2
 3,2,1 -> 1,2,3
 1,1,5 -> 1,5,1
+
+Follow up:
+Get the previous permutation in the sequence
+
 '''
 
 class Solution:
@@ -20,20 +24,49 @@ class Solution:
         size = len(num)
         if size < 2:
             return
-        for pos1 in range(size-1, -1, -1):
-            for pos2 in range(pos1, -1, -1):
-                if num[pos1] > num[pos2]:
-                    num[pos1], num[pos2] = num[pos2], num[pos1]
-                    num[pos2+1:] = sorted(num[pos2+1:])
-                    return
-        num.sort()
+        max = num[-1]
+        pos = size - 1
+        while pos >= 0:
+            if max > num[pos]:
+                break
+            max = num[pos]
+            pos -= 1
+        self.reverse(num, pos + 1, size - 1)
+        if pos == -1:
+            return
+        for i in range(pos+1, size):
+            if num[i] > num[pos]:
+                num[i], num[pos] = num[pos], num[i]
+                break
+        return
+
+    def previousPermutation(self, num):
+        size = len(num)
+        if size < 2:
+            return
+        min = num[-1]
+        for pos in range(size-1, -1, -1):
+            if min < num[pos]:
+                break
+            min = num[pos]
+        self.reverse(num, pos + 1, size - 1)
+        if pos == -1:
+            return
+        for index in range(pos+1, size):
+            if num[index] < num[pos]:
+                num[index], num[pos] = num[pos], num[index]
+                break
+        return
+
+    def reverse(self, num, start, end):
+        while start < end:
+            num[start], num[end] = num[end], num[start]
+            start += 1
+            end -= 1
 
 if __name__ == '__main__':
     solution = Solution()
-    num = [1,1,5]
+    num = [4,2,0,2,3,2,0]
     solution.nextPermutation(num)
-    print num
-    num = [1,3,2]
-    num = [3,2,1]
-    solution.nextPermutation(num)
+    solution.previousPermutation(num)
     print num
